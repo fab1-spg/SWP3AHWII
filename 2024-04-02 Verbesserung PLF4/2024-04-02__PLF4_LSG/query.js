54832ff0147b3b1b47c16d7656c560a3d95234d8
fab1-spg <FIN210321@spengergasse.at>
2024-04-15 21:41:39 +0200

    .

--- 2024-04-02 Verbesserung PLF4/2024-04-02__PLF4_LSG/query.js
+++ 2024-04-02 Verbesserung PLF4/2024-04-02__PLF4_LSG/query.js
@@ -1,4 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+async function getWatchlistNamesByUser(userId) {
+    const user = await prisma.benutzer.findUnique({
+      where: { id: userId },
+      include: { watchLists: { select: { name: true } } },
+    });
+    return user.watchLists.map(watchlist => watchlist.name);
+  }
+  
+
 
