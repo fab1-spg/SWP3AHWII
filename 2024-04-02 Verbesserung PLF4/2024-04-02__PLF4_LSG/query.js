ec8787e377e4f162fa111b34cbb63d73b61a4a2f
maniARJ1 <arf22297@spengergasse.at>
2024-04-22 20:03:11 +0200

    .

--- 2024-04-02 Verbesserung PLF4/2024-04-02__PLF4_LSG/query.js
+++ 2024-04-02 Verbesserung PLF4/2024-04-02__PLF4_LSG/query.js
@@ -1,6 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+async function getWatchlistsByTrackName(trackName) {
+  const watchlists = await prisma.watchlistItem.findMany({
+      where: { song: { name: trackName } },
+      include: { watchlist: { include: { user: true } } },
+  });
+
+
+
 async function getWatchlistNamesByUser(userId) {
     const user = await prisma.benutzer.findUnique({
       where: { id: userId },
